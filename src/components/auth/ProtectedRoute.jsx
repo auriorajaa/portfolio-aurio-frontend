14ec003bdb07becc24c0a95a0875b38d4168f2ca
auriorajaa <riorajaa2018@gmail.com>
2026-02-08 18:15:49 +0700

    update import statements warning

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import { Box, Spinner, Center } from "@chakra-ui/react";
+import { Spinner, Center } from "@chakra-ui/react";
 
 const ProtectedRoute = ({ children }) => {
   const { currentUser, isAdminUser, loading } = useAuth();
